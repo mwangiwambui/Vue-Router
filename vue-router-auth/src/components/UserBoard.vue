295e219a6f389543dd58eaeeec84b443e2b0a357
mwangiwambui <wambui54mwangi@gmail.com>
2021-11-22 22:17:36 +0300

    Initial code

---
+++ vue-router-auth/src/components/UserBoard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to the users page</h1>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Authenticated Users'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
